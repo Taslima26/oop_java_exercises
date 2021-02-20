6516d1f56d945a4bc3f02d674c0595ba68ae581a
taslima26 <banglawalas@hotmail.co.uk>
2021-02-20 09:50:57 +0000

    refactored code after James comment

--- src/main/java/com/techreturners/cats/AbstractCat.java
+++ src/main/java/com/techreturners/cats/AbstractCat.java
@@ -1,6 +1,6 @@
 package com.techreturners.cats;
 
-public class AbstractCat implements Cat{
+public abstract class AbstractCat implements Cat{
     private boolean isSleep=false;
     private String catSetting;
     private int avgHeight;
--- src/main/java/com/techreturners/cats/LionCat.java
+++ src/main/java/com/techreturners/cats/LionCat.java
@@ -1,11 +1,6 @@
 package com.techreturners.cats;
-
-//call WildCat constructor
 public class LionCat extends AbstractCat {
     public LionCat() {
         super(true,"LionCat",1100,"Roar!!!!");
     }
-
-    
-   
 } 
